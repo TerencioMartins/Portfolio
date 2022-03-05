8aed3a1d1a3d835ff3aa6e8cff1392f86d91db89
Terencio Martins <zerofirty0@gmail.com>
2022-03-05 12:04:07 -0300

    Update

--- src/components/particlesConfig/particlesBackground.jsx
+++ src/components/particlesConfig/particlesBackground.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
-import Particles from "react-tsparticles";
-import { useState } from "react";
+import React from "react";
+import Particles from "react-tsparticles"
+
 
 export default function ParticleBackground() {
 
